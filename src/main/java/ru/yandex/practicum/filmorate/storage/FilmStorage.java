54f7ed1c933641fa1a2b5a3bd094552dbf534fad
neongogo92 <anton.neongogo@yandex.ru>
2023-10-16 03:43:35 +0300

    Исправил все замечания, кроме объединения таблиц в методе Film findById. Не хватает времени разобраться, почему после оптимизации запросов, не проходит тестирование в postman. Это критичное замечание? Я не успеваю уложиться в жесткий дедлайн перед групповым проектом.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,11 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FilmStorage {
   Film add(Film film);
